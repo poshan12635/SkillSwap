09e6526755f2d8b87c333cb7c7f741440d948523
poshan <karkiposhan9@gmail.com>
2025-07-15 21:34:31 +0545

    added neon

--- frontend/skillswapfont/src/pages/Navbar1.jsx
+++ frontend/skillswapfont/src/pages/Navbar1.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import skill from "/src/assets/skill.png";
 import useri from "/src/assets/useri.png";
 import './Navbar1.css'
+import Submenu from "./Submenu";
 
 
 export default function Navbar1() {
@@ -13,9 +14,14 @@ export default function Navbar1() {
             </div>
             <ul className="navbarlink">
                 <li>
-                    <button>
+                    <button onClick={<Submenu />}>
                         <img src={useri} className="profileimage" />
                     </button>
+
+
+
+
+
                 </li>
             </ul>
 
---
+++ frontend/skillswapfont/src/pages/Submenu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Submenu() {
+    return (
+        <div className="submenu">
+            <img src={useri} className="profileimage" />
+            <ul>
+                <li><a href="" />Profile</li>
+                <li><a href="" />message</li>
+                <li><a href="" />Signout</li>
+            </ul>
+        </div>
+    );
+}
